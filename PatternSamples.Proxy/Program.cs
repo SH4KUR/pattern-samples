bfb08dc8aab209316e0f13730b5ca324e697f0b8
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-10-10 18:48:40 +0300

    Add Proxy project

---
+++ PatternSamples.Proxy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace PatternSamples.Proxy
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Proxy: Supervision!\n");
+
+            var supervision = new SupervisionProxy(new Room());
+
+            var person1 = new Person("Nick");
+            var person2 = new Person("John");
+
+            Console.WriteLine(supervision.Enter(person1));
+            Thread.Sleep(1000);
+            Console.WriteLine(supervision.Exit(person1));
+            Thread.Sleep(1000);
+            Console.WriteLine(supervision.Enter(person2));
+            Thread.Sleep(1000);
+            Console.WriteLine(supervision.Exit(person2));
+            
+            Console.Write("\nPress <ENTER>:");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PatternSamples.Proxy/Supervision.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace PatternSamples.Proxy
+{
+    public interface IRoom
+    {
+        string Enter(Person person);
+        string Exit(Person person);
+    }
+
+    public class Room : IRoom
+    {
+        public string Enter(Person person)
+        {
+            return $"{person.Name} enter room...";
+        }
+
+        public string Exit(Person person)
+        {
+            return $"{person.Name} exit room...";
+        }
+    }
+
+    public class SupervisionProxy : IRoom
+    {
+        private IRoom _room;
+
+        public SupervisionProxy(IRoom room)
+        {
+            _room = room;
+        }
+
+        public string Enter(Person person)
+        {
+            return $"{DateTime.Now.ToLongTimeString()} : {_room.Enter(person)}";
+        }
+
+        public string Exit(Person person)
+        {
+            return $"{DateTime.Now.ToLongTimeString()} : {_room.Exit(person)}";
+        }
+    }
+
+    public class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
