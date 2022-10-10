427e971e7a905abd1736e850e2acc56c76e3bb14
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-10-10 17:57:14 +0300

    Add adapter project

---
+++ PatternSamples.Adapter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PatternSamples.Adapter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Adapter: Stranger Things!\n");
+
+            var veryStrange = new VeryStrangerThings();
+            var strangeAdapter = new Adapter(veryStrange);
+
+            Console.WriteLine(strangeAdapter.GetStrange());
+            
+            Console.Write("\nPress <ENTER>:");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PatternSamples.Adapter/VeryStrangerThings.cs
@@ -0,0 +1,39 @@
+﻿namespace PatternSamples.Adapter
+{
+    public interface IStrangerThings
+    {
+        string GetStrange();
+    }
+
+    //------------
+    
+    public interface IVeryStrangerThings
+    {
+        string GetVeryStrange();
+    }
+
+    public class VeryStrangerThings : IVeryStrangerThings
+    {
+        public string GetVeryStrange()
+        {
+            return "VeryStrangerThings message";
+        }
+    }
+
+    //------------
+    
+    public class Adapter : IStrangerThings
+    {
+        private IVeryStrangerThings _adaptee;
+
+        public Adapter(IVeryStrangerThings adaptee)
+        {
+            _adaptee = adaptee;
+        }
+
+        public string GetStrange()
+        {
+            return _adaptee.GetVeryStrange();
+        }
+    }
+}
