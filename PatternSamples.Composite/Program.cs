cef00930c4d557c67702b7118758107a7f8a1fa3
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-10-10 13:15:19 +0300

    Add empty Composite project

---
+++ PatternSamples.Composite/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatternSamples.Composite
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
