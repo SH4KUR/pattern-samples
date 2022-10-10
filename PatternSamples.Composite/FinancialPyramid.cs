3bf06c69d477ec2863d40dfc1f787603d8a2e711
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-10-10 14:03:56 +0300

    Added Composite project

---
+++ PatternSamples.Composite/FinancialPyramid.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PatternSamples.Composite
+{
+    public interface IFinancialPyramid
+    {
+        decimal CountMoney();
+    }
+
+    public class Person : IFinancialPyramid
+    {
+        private readonly decimal _money;
+
+        public Person(decimal money)
+        {
+            _money = money;
+        }
+
+        public decimal CountMoney()
+        {
+            return _money;
+        }
+    }
+    
+    public class FinancialGroup : IFinancialPyramid
+    {
+        private readonly List<IFinancialPyramid> _members = new List<IFinancialPyramid>();
+
+        public void Add(IFinancialPyramid financialMember)
+        {
+            _members.Add(financialMember);
+        }
+        
+        public decimal CountMoney()
+        {
+            return _members.Sum(m => m.CountMoney());
+        }
+    }
+}
--- PatternSamples.Composite/Program.cs
+++ PatternSamples.Composite/Program.cs
@@ -6,7 +6,23 @@ namespace PatternSamples.Composite
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Composite: Financial Pyramid!\n");
+
+            var sub = new FinancialGroup();
+            sub.Add(new Person(150));           // 450
+            sub.Add(new Person(250));
+            sub.Add(new Person(50));
+
+            var main = new FinancialGroup();
+            main.Add(new Person(100));          // 350
+            main.Add(new Person(200));
+            main.Add(new Person(50));
+            main.Add(sub);
+
+            Console.WriteLine($"Sum: {main.CountMoney()}");     // 800
+            
+            Console.Write("\nPress <ENTER>:");
+            Console.ReadLine();
         }
     }
 }
