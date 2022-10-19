ec363917a577c4fe649229d3059248f440ff5c11
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-10-19 19:54:09 +0300

    Add Observer project

---
+++ PatternSamples.Observer/Crossroads.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace PatternSamples.Observer
+{
+    public interface IObserver
+    {
+        void LookTrafficLight(ICrossroads crossroads);
+    }
+    
+    public interface ICrossroads
+    {
+        void CarArrived(IObserver transport);
+        void CarLeft(IObserver transport);
+        void ChangeTrafficLightSignal();
+        TrafficLightSignal GetTrafficLightSignal();
+    }
+
+    public enum TrafficLightSignal { Red, Green }
+
+    public class Crossroads : ICrossroads
+    {
+        private readonly IList<IObserver> _transports = new List<IObserver>();
+        
+        public TrafficLightSignal Signal { get; set; }  = TrafficLightSignal.Red;
+
+        public void CarArrived(IObserver transport)
+        {
+            _transports.Add(transport);
+            transport.LookTrafficLight(this);
+        }
+
+        public void CarLeft(IObserver transport) => _transports.Remove(transport);
+
+        public void ChangeTrafficLightSignal()
+        {
+            Signal = Signal == TrafficLightSignal.Green
+                ? TrafficLightSignal.Red 
+                : TrafficLightSignal.Green;
+            
+            foreach (var transport in _transports)
+            {
+                transport.LookTrafficLight(this);
+            }
+        }
+
+        public TrafficLightSignal GetTrafficLightSignal() => Signal;
+    }
+
+    public class Transport : IObserver
+    {
+        public bool IsAllowed { get; set; }
+
+        public void LookTrafficLight(ICrossroads crossroads)
+        {
+            IsAllowed = crossroads.GetTrafficLightSignal() == TrafficLightSignal.Green;
+        }
+    }
+}
---
+++ PatternSamples.Observer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PatternSamples.Observer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Observer: Crossroads!\n");
+
+            var crossroads = new Crossroads();
+
+            var car1 = new Transport();
+            var car2 = new Transport();
+            var truck = new Transport();
+            
+            crossroads.CarArrived(car1);
+            crossroads.CarArrived(car2);
+            crossroads.CarArrived(truck);
+
+            Console.WriteLine($"Traffic Light: {crossroads.Signal}");
+            Console.WriteLine($"{nameof(car1)}: {car1.IsAllowed}");
+            Console.WriteLine($"{nameof(car2)}: {car2.IsAllowed}");
+            Console.WriteLine($"{nameof(truck)}: {truck.IsAllowed}");
+
+            crossroads.ChangeTrafficLightSignal();
+
+            Console.WriteLine($"\nTraffic Light: {crossroads.Signal}");
+            Console.WriteLine($"{nameof(car1)}: {car1.IsAllowed}");
+            Console.WriteLine($"{nameof(car2)}: {car2.IsAllowed}");
+            Console.WriteLine($"{nameof(truck)}: {truck.IsAllowed}");
+
+            Console.Write("\nPress <ENTER>:");
+            Console.ReadLine();
+        }
+    }
+}
