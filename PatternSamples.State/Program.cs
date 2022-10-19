903d9555eaf8ff6ff01503f1faa4dfa2a5f34062
SH4KUR <dmitriy.lopatiev@gmail.com>
2022-10-19 19:14:43 +0300

    Add State project

---
+++ PatternSamples.State/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PatternSamples.State
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("State: Traffic Light!\n");
+
+            var trafficLight = new TrafficLight(new RedSignal());
+
+            Console.WriteLine($": {trafficLight.LetCarGo()}");
+            Console.WriteLine($": {trafficLight.LetCarGo()}");
+            Console.WriteLine($": {trafficLight.LetCarGo()}");
+            
+            Console.Write("\nPress <ENTER>:");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PatternSamples.State/TrafficLight.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace PatternSamples.State
+{
+    public class TrafficLight
+    {
+        private Signal _signal;
+
+        public TrafficLight(Signal signal)
+        {
+            ChangeSignal(signal);
+        }
+
+        public void ChangeSignal(Signal signal)
+        {
+            _signal = signal;
+            _signal.SetTrafficLight(this);
+        }
+
+        public bool LetCarGo()
+        {
+            return _signal.LetCarGo();
+        }
+    }
+
+    public abstract class Signal
+    {
+        protected TrafficLight _trafficLight;
+        
+        public void SetTrafficLight(TrafficLight trafficLight)
+        {
+            _trafficLight = trafficLight;
+        }
+
+        public abstract bool LetCarGo();
+    }
+
+    public class RedSignal : Signal
+    {
+        public override bool LetCarGo()
+        {
+            Console.Write(nameof(RedSignal));
+            
+            _trafficLight.ChangeSignal(new YellowSignal());
+            return false;
+        }
+    }
+
+    public class YellowSignal : Signal
+    {
+        public override bool LetCarGo()
+        {
+            Console.Write(nameof(YellowSignal));
+            
+            _trafficLight.ChangeSignal(new GreenSignal());
+            return false;
+        }
+    }
+
+    public class GreenSignal : Signal
+    {
+        public override bool LetCarGo()
+        {
+            Console.Write(nameof(GreenSignal));
+            
+            _trafficLight.ChangeSignal(new RedSignal());
+            return true;
+        }
+    }
+}
